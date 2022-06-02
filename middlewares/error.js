a7612e23286e992ad2ee67015ae893ae4dca64c3
NarawitC <narawit.developer@outlook.com>
2022-06-02 14:08:45 +0700

    middlewares-authentucate

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,3 +1,18 @@
 module.exports = (err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  console.log(err);
+
+  if (err.name === 'TokenExpiredError') {
+    err.statusCode = 401;
+  }
+  if (err.name === 'JsonWebTokenError') {
+    err.statusCode = 401;
+  }
+  if (
+    err.name === 'SequelizeValidationError' ||
+    err.name === 'SequelizeUniqueConstraintError'
+  ) {
+    err.message = err.errors[0].message;
+    err.statusCode = 400;
+  }
+  res.status(err.status || 500).json({ message: err.message });
 };
