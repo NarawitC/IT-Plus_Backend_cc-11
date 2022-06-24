14460322e60b9dc58e8965e21f2cd07944aae2f9
NarawitC <narawit.developer@outlook.com>
2022-06-24 15:57:50 +0700

    COMPLETED

---
+++ middlewares/supplier/authenticate.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../../models');
+
+const createError = require('../error');
+exports.supplierAuthenticate = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith('Bearer')) {
+      createError('You are unauthorized', 401);
+    }
+    const token = authorization.split(' ')[1];
+    if (!token) {
+      createError('You are unauthorized', 401);
+    }
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const user = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { id: payload.id, role: payload.role },
+    });
+    if (!user) {
+      createError('You are unauthorized', 401);
+    }
+    req.supplier = supplier;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
