6891063eb0232472c094a889c4f3dde7e15cf6e5
NarawitC <narawit.developer@outlook.com>
2022-06-26 14:46:17 +0700

    COMPLETED

--- routes/admin/adminProductRoutes.js
+++ routes/admin/adminProductRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const adminProductController = require('../../controllers/admin/adminProductController');
 
 router.get('/', adminProductController.getAllProduct);
+router.get('/:productId', adminProductController.getProductById);
+router.patch('/approve/:productId', adminProductController.approveProduct);
 
 module.exports = router;
