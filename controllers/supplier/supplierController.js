7a81d00fbd11628c47fa369e752055ebcb4b82a0
NarawitC <narawit.developer@outlook.com>
2022-06-26 21:43:21 +0700

    COMPLETED

--- controllers/supplier/supplierController.js
+++ controllers/supplier/supplierController.js
@@ -1,20 +1,20 @@
-const { User, Client } = require('../../models');
+const { User, Supplier } = require('../../models');
 const { USER_ROLE } = require('../../config/constants');
 
-exports.getClientInfo = async (req, res, next) => {
+exports.getSupplierInfo = async (req, res, next) => {
   try {
     const user = await User.findOne({
       attributes: {
         exclude: ['password'],
-        where: { id: req.user.id, role: USER_ROLE.CLIENT },
+        where: { id: req.user.id, role: USER_ROLE.SUPPLIER },
       },
-      include: [{ model: Client }],
+      include: [{ model: Supplier }],
     });
     if (!user) {
       createError('You are unauthorize', 404);
     }
     res.status(200).json({
-      message: 'Get client info successfully',
+      message: 'Get supplier info successfully',
       user,
     });
   } catch (err) {
