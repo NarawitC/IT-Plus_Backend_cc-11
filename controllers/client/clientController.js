fb1f219def7f9ae14b9f9b8e9acde0fc225dfbfc
NarawitC <narawit.developer@outlook.com>
2022-06-27 15:41:20 +0700

    COMPLETED

--- controllers/client/clientController.js
+++ controllers/client/clientController.js
@@ -21,3 +21,66 @@ exports.getClientInfo = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.updateClientInfo = async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      phoneNumber,
+      email,
+      password,
+      confirmPassword,
+      address,
+    } = req.body;
+
+    if (email) {
+      const isEmail = validator.isEmail('' + email);
+      if (!isEmail) {
+        createError('Email is invalid format', 400);
+      }
+    }
+
+    if (password || confirmPassword) {
+      if (password !== confirmPassword) {
+        createError('Password did not match', 400);
+      }
+    }
+
+    if (password) {
+      const hashedPassword = await bcryptjs.hash(password, 12);
+      password = hashedPassword;
+    }
+
+    if (phoneNumber) {
+      const isPhoneNumber = validator.isMobilePhone('' + phoneNumber, 'th-TH');
+      if (!isPhoneNumber) {
+        createError('PhoneNumber is invalid format', 400);
+      }
+    }
+
+    const updated = await User.update(
+      {
+        firstName,
+        lastName,
+        phoneNumber,
+        email,
+        password,
+        address,
+      },
+      {
+        where: { id: req.user.id },
+      }
+    );
+
+    if (updated[0] === 0) {
+      createError('You are unauthorize', 404);
+    }
+
+    res.status(200).json({
+      message: 'Update client info successfully',
+    });
+  } catch (err) {
+    next(err);
+  }
+};
