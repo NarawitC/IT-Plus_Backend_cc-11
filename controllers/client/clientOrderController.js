c6a8568c626f68e1f673233c5457630b0a9e9a21
NarawitC <narawit.developer@outlook.com>
2022-06-29 15:36:13 +0700

    COPLETED

--- controllers/client/clientOrderController.js
+++ controllers/client/clientOrderController.js
@@ -1,4 +1,11 @@
-const { Cart, Order, OrderItem, CartItem, sequelize } = require('../../models');
+const {
+  Cart,
+  Order,
+  OrderItem,
+  Product,
+  sequelize,
+  Promotion,
+} = require('../../models');
 const { Op } = require('sequelize');
 
 exports.bulkCreateOrderWithOrderItem = async (req, res, next) => {
@@ -42,6 +49,54 @@ exports.bulkCreateOrderWithOrderItem = async (req, res, next) => {
     });
   } catch (error) {
     await transaction.rollback();
+    next(error);
+  }
+};
+
+exports.getAllOrder = async (req, res, next) => {
+  try {
+    const {
+      Client: { id: clientId },
+    } = req.user;
+    const orders = await Order.findAll({
+      where: {
+        clientId,
+      },
+      include: [
+        {
+          model: OrderItem,
+          include: [{ model: Product, include: [{ model: Promotion }] }],
+        },
+      ],
+    });
+    res.status(200).json({
+      message: 'Get all order successfully',
+      orders,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getOrderById = async (req, res, next) => {
+  try {
+    const { orderId } = req.params;
+    const order = await Order.findOne({
+      where: {
+        id: orderId,
+      },
+      include: [
+        {
+          model: OrderItem,
+          include: [{ model: Product, include: [{ model: Promotion }] }],
+        },
+      ],
+    });
+    res.status(200).json({
+      message: 'Get order successfully',
+      order,
+    });
+  } catch (error) {
     next(error);
   }
 };
