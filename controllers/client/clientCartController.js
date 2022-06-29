644f443e2feee262fc9e9351db499a6038974843
NarawitC <narawit.developer@outlook.com>
2022-06-29 13:59:24 +0700

    COPLETED

--- controllers/client/clientCartController.js
+++ controllers/client/clientCartController.js
@@ -1,4 +1,4 @@
-const { Cart } = require('../../models');
+const { Cart, CartItem, Product, Promotion } = require('../../models');
 const { Op } = require('sequelize');
 
 exports.createCart = async (req, res, next) => {
@@ -12,6 +12,31 @@ exports.createCart = async (req, res, next) => {
     res.status(200).json({
       message: 'Create cart successfully',
       cart,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAllCart = async (req, res, next) => {
+  try {
+    const {
+      Client: { id: clientId },
+    } = req.user;
+    const carts = await Cart.findAll({
+      where: {
+        clientId,
+      },
+      include: [
+        {
+          model: CartItem,
+          include: [{ model: Product, include: [{ model: Promotion }] }],
+        },
+      ],
+    });
+    res.status(200).json({
+      message: 'Get all cart successfully',
+      carts,
     });
   } catch (error) {
     next(error);
