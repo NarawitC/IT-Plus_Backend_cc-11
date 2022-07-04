6af0d60d3bba5004743f1690502a7b6595756296
NarawitC <narawit.developer@outlook.com>
2022-07-04 23:24:49 +0700

    COMPLETED

--- controllers/client/clientPurchasedOrderController.js
+++ controllers/client/clientPurchasedOrderController.js
@@ -3,24 +3,52 @@ const {
   CartItem,
   Product,
   Promotion,
+  Order,
   PurchasedOrder,
+  OrderItem,
+  sequelize,
 } = require('../../models');
 const { Op } = require('sequelize');
 
-exports.createPurchaseOrder = async (req, res, next) => {
+exports.createPurchasedOrder = async (req, res, next) => {
+  const transaction = await sequelize.transaction();
   try {
-    console.log('createPurchaseOrder');
-    const { orderId } = req.params;
-    const { paymentAt, transactionId } = req.body;
-    const purchasedOrder = await PurchasedOrder.create({
-      orderId,
-      paymentAt,
-      transactionId,
+    const { paymentAt, transactionId, orderIds } = req.body;
+    const bulkData = [];
+    orderIds.forEach((orderId) => {
+      bulkData.push({ orderId, paymentAt, transactionId });
     });
-    res
-      .status(200)
-      .json({ message: 'Create purchased order successfully', purchasedOrder });
+
+    const purchasedOrders = await PurchasedOrder.bulkCreate(bulkData, {
+      transaction,
+    });
+
+    const orderItems = await OrderItem.findAll({
+      where: { orderId: { [Op.in]: orderIds } },
+    });
+    const productIdsAndQuantity = orderItems.map((orderItem) => {
+      return { productId: orderItem.productId, quantity: +orderItem.quantity };
+    });
+    const productIds = productIdsAndQuantity.map((productIdAndQuantity) => {
+      return productIdAndQuantity.productId;
+    });
+    const products = await Product.findAll({
+      where: { id: { [Op.in]: productIds } },
+    });
+    products.forEach((product) => {
+      product.stock -= productIdsAndQuantity.find((productIdAndQuantity) => {
+        return productIdAndQuantity.productId === product.id;
+      }).quantity;
+    });
+    await Promise.all(products.map((product) => product.save({ transaction })));
+
+    transaction.commit();
+    res.status(200).json({
+      message: 'Create purchased order successfully',
+      purchasedOrders,
+    });
   } catch (error) {
+    transaction.rollback();
     next(error);
   }
 };
